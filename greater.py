a79f22b9143c4c825629dda492d128684fd64f27
Akshay8129 <akshaymohan8129@gmail.com>
2021-11-25 15:51:01 +0530

    first commit

---
+++ greater.py
@@ -0,0 +1,7 @@
+x=int(input("enter the first number"))
+x1=int(input("enter the second number"))
+if(x>x1):
+    print(f"the greater number is{x}")
+else:
+    print(f"the greater number is {x1}")
+    
---
+++ smallestof3.py
@@ -0,0 +1,11 @@
+x=int(input("enter the first number"))
+x1=int(input("enter the second number"))
+x2=int(input("enter the third number"))
+if(x<x1):
+    print(f"the smallest number is{x}")
+elif(x1<x2):
+    print(f"the smallest number is {x1}")
+else:
+    print(f"the smallest number is {x2}")
+    
+    
